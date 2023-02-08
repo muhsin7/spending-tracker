9793e712ae1eebef804e3c1565561eb501ec6455
Huy Phan <duc.phan@kcl.ac.uk>
2023-02-08 23:45:12 +0000

    Add login form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,19 @@
+import React from "react";
+import './Login.css';
+
 export default function Login() {
-    return (
-        <form>
-            <h1>Login</h1>
-        </form>
-    )
+        return(
+            <div className="div-login">
+                <h2>Login</h2>
+                <div>
+                    <form>
+                        <input type='email' name='email' placeholder="Email:" required/>
+                        <input type='password' name='password' placeholder="Password:" required/>
+                        <button type="submit" id="logInButton" >Log In</button>
+                        <button id = "registerButton" >Don't have an account? Register here!</button>
+                    </form>
+                </div>
+            </div>
+        )
+    
 }
