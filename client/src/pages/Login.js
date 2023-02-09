c12451262c4a6a93edd575b56b352225b730818f
Huy Phan <duc.phan@kcl.ac.uk>
2023-02-09 13:11:35 +0000

    Add forgot password button and redirect to register page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,15 @@
 import React from "react";
 import './Login.css';
+import { Navigate } from "react-router-dom";
 
-export default function Login() {
+function Login() {
+
+        const [goToRegister, setGoToRegister] = React.useState(false);
+
+        if(goToRegister){
+            return<Navigate to="/register"/>;
+        }
+
         return(
             <div className="div-login">
                 <h2>Login</h2>
@@ -9,11 +17,16 @@ export default function Login() {
                     <form>
                         <input type='email' name='email' placeholder="Email:" required/>
                         <input type='password' name='password' placeholder="Password:" required/>
+                        <button id="forgotPassword">Forgot your password?</button>
                         <button type="submit" id="logInButton" >Log In</button>
-                        <button id = "registerButton" >Don't have an account? Register here!</button>
+                        <button onClick={() => {
+                            setGoToRegister(true);
+                        }}
+                            id = "registerButton" >Don't have an account? Register here!</button>
                     </form>
                 </div>
             </div>
         )
-    
 }
+
+export default Login;
