6dd2a18cf5a499d831e22596e80e940155043cda
Quan Tran <quanabcis2003@gmail.com>
2023-02-20 14:12:56 +0000

    Adjust tests to adhere to error reporting and export Mocha configuration to external file.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -25,24 +25,32 @@ const registerUser = asyncHandler(async (req, res) => {
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
 
-  const user = await User.create({
-    name,
-    email,
-    password: hashedPassword
-  });
-
-  if(user) {
-    res.status(201).json({
-      _id: user.id,
-      name: user.name,
-      email: user.email,
-      token: generateToken(user.id)
-
+  try{
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword
     });
-  } else {
-    res.status(400);
-    throw new Error("Invalid user data");
+
+    if(user) {
+      res.status(201).json({
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user.id)
+  
+      });
+    } else {
+      res.status(400);
+      throw new Error("Invalid user data");
+    }
   }
+  //Catch validation error if form data is invalid
+  catch(error) {
+    res.status(400);
+    throw error;
+  }
+  
 });
 
 // @desc    Log in existing user
