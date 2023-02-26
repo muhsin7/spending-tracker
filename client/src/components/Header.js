3e9e852da14a10aa46e71dc5c273f54ef5074c18
Josh Wong <jorywo@gmail.com>
2023-02-26 15:08:06 +0000

    Add Header to Dashboard

---
+++ client/src/components/Header.js
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom'
+import "./header.css"
+
+export default function Header() {
+
+    const onLogout = () => {
+        window.alert("Logout")
+    }
+
+    return (
+        <header className='header'>
+          <div className='logo'>
+            <Link to='/'>Dashboard</Link>
+          </div>
+          <ul>
+              <li>
+                <button className='btn btn-header' onClick={onLogout}>
+                    Logout
+                </button>
+              </li>
+          </ul>
+        </header>
+      )
+}
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,7 +1,12 @@
+import Header from "../components/Header"
+
 export default function Dashboard() {
     return (
-        <form>
-            <h1>Dashboard</h1>
-        </form>
+        <>
+            < Header/>
+            <form>
+                <h1>Dashboard</h1>
+            </form>
+        </>
     )
 }
