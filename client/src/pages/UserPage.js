9b2ff40b89a0dce05c27aec929bbe899635d55bf
muhsin <muhsin.ma.mohamed@gmail.com>
2023-02-27 15:23:02 +0000

    Create schema, controller, routes for spending limits

--- client/src/pages/UserPage.js
+++ client/src/pages/UserPage.js
@@ -1,14 +1,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useToken } from "../Authentication/useToken";
-import { useUser } from "../Authentication/useUser";
 
 export default function UserPage() {
   const [token, setToken] = useToken();
   const [user, setUser] = useState({});
 
   useEffect(() => {
-    const response = axios.get('/api/user/profile', {
+    axios.get('/api/user/profile', {
         headers: {
             "Authorization": "Bearer " + token
         }
