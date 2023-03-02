6be8e8d790ef5f5b88f3f8c772ba808bc02fe764
muhsin <muhsin.ma.mohamed@gmail.com>
2023-03-02 21:39:17 +0000

    Create transaction list component and improve amount component in dashboard

--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -1,36 +1,16 @@
 import { useState } from "react";
+import AmountSpent from "./AmountSpent";
+import TransactionsPreview from "./TransactionsPreview";
 
 export default function Dashboard() {
 
-    const durations = ["day", "week", "month", "year"];
-
-    const [dropdownDuration, setDropdownDuration] = useState(durations[0]);
-
-    const durationElements = [];
-
     
-    durations.forEach(length => {
-        durationElements.push(<option value={length}>{length==="day" ? "today" : length}</option>)
-    });
-
-    function setOption(event) {
-        setDropdownDuration(event.target.value);
-    }
 
     return (
         <div className="dashboard">
             <h1>Dashboard</h1>
-            <div className="dashboard-money">
-                <div className="dashboard-amount">£ 42</div>
-                <div className="dashboard-amount-description">
-                    <form>
-                        <label for="duration">spent { dropdownDuration === "day" ? "" : "this" } </label>
-                        <select onChange={setOption} id="amount-duraton" name="duration">
-                            { durationElements }
-                        </select>
-                    </form>
-                </div>
-            </div>
+            <AmountSpent />
+            <TransactionsPreview /> 
         </div>
     )
 }
