dea455a70b797433b08b26275fe9e50ef2cc817d
Josh Wong <jorywo@gmail.com>
2023-03-03 00:20:24 +0000

    Make Header move fluidly

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -2,11 +2,11 @@ import Header from "../components/Header"
 
 export default function Dashboard() {
     return (
-        <>
+        <div className="header-container">
             < Header/>
             <form>
                 <h1>Dashboard</h1>
             </form>
-        </>
+        </div>
     )
 }
