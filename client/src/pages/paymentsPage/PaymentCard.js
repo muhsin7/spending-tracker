0227a2c42ae5e44da3d0ff408fcdcb345fa9183e
Raymond Cheung <raymond.cheung@kcl.ac.uk>
2023-03-08 23:42:23 +0000

    Create a popup when payment button is clicked

--- client/src/pages/paymentsPage/PaymentCard.js
+++ client/src/pages/paymentsPage/PaymentCard.js
@@ -1,14 +1,22 @@
+import React from "react";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
+
 export default function PaymentCard(props) {
-  let title = props.payment.title;
-  let date = "THIS IS A PLACEHOLDER FOR THE DATE";
-  let description = props.payment.description;
-  let doesImageExist = props.payment.hasOwnProperty('image');
-
+  const TITLE = props.payment.title;
+  const DATE = "THIS IS A PLACEHOLDER FOR THE DATE";
+  const DESCRIPTION = props.payment.description;
+  const DOES_IMAGE_EXIST = props.payment.hasOwnProperty('image');
+  
   // Rounds the price to 2 d.p.
-  let price = (Math.round(props.payment.amount * 100) / 100).toFixed(2);
+  const PRICE = (Math.round(props.payment.amount * 100) / 100).toFixed(2);
 
-  function displayImage() {
-    let image = props.payment.image;
+  let imageURL = "";
+
+  if (DOES_IMAGE_EXIST) {
+    const IMAGE = props.payment.image;
+    imageURL = `data:${IMAGE.contentType};base64,${IMAGE.data.toString('base64')}`;
+    console.log(imageURL);
     // DISPLAY THE IMAGE
   }
 
@@ -16,14 +24,16 @@ export default function PaymentCard(props) {
     <div className="payment-card">
       <div className="payment-info">
         <div className="payment-card-top">
-          <span className="payment-title">{title}</span>
-          <span className="payment-amount">{"-£" + price}</span>
+          <span className="payment-title">{TITLE}</span>
+          <span className="payment-amount">{"-£" + PRICE}</span>
         </div>
-        <span className="payment-description">{description}</span>
+        <span className="payment-description">{DESCRIPTION}</span>
         <div className="payment-card-bottom">
-          <span className="payment-date">{date}</span>
-          {doesImageExist && (
-            <button className="payment-image-button" onClick={displayImage}>View image</button>
+          <span className="payment-date">{DATE}</span>
+          {DOES_IMAGE_EXIST && (
+            <Popup trigger={<button className="payment-image-button">View image</button>} position="left" contentStyle={{ width: 'auto'}}>
+              <img src={imageURL} />
+            </Popup>
           )}
         </div>
       </div>
