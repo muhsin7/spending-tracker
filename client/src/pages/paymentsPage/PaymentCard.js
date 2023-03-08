ff1d59dc831b65b6afbcda85e45665236015f86b
Raymond Cheung <raymond.cheung@kcl.ac.uk>
2023-03-08 21:54:22 +0000

    Show the button only if payment contains an image

--- client/src/pages/paymentsPage/PaymentCard.js
+++ client/src/pages/paymentsPage/PaymentCard.js
@@ -2,12 +2,14 @@ export default function PaymentCard(props) {
   let title = props.payment.title;
   let date = "THIS IS A PLACEHOLDER FOR THE DATE";
   let description = props.payment.description;
+  let doesImageExist = props.payment.hasOwnProperty('image');
 
   // Rounds the price to 2 d.p.
   let price = (Math.round(props.payment.amount * 100) / 100).toFixed(2);
 
   function displayImage() {
-
+    let image = props.payment.image;
+    // DISPLAY THE IMAGE
   }
 
   return (
@@ -20,7 +22,9 @@ export default function PaymentCard(props) {
         <span className="payment-description">{description}</span>
         <div className="payment-card-bottom">
           <span className="payment-date">{date}</span>
-          <button className="payment-image-button" onClick={displayImage}>View image</button>
+          {doesImageExist && (
+            <button className="payment-image-button" onClick={displayImage}>View image</button>
+          )}
         </div>
       </div>
     </div>
