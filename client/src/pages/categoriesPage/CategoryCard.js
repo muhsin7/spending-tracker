61e3e108cd381427eff23ed48b58c72c4132222a
Huy Phan <duc.phan@kcl.ac.uk>
2023-03-10 01:50:43 +0000

    Add delete function to category card

--- client/src/pages/categoriesPage/CategoryCard.js
+++ client/src/pages/categoriesPage/CategoryCard.js
@@ -1,16 +1,34 @@
 import React from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 
-function CategoryCard() {
-    return (
-        <div className="category-card">
-            <div className="category-info">
-                <div class="category-title">Food</div>
-            </div>
-            <div className="category-spending-limit">Spending Limit: $400</div>
-            <button className="category-card-button"> Edit </button>
-            <button className="category-card-button"> Delete </button>
+function CategoryCard(props) {
 
+  const ID = props.category.id;
+  const TITLE = props.category.title;
+  const SPENDINGLIMIT = props.category.spendingLimit
+
+
+  const deleteCategory = async () => {
+    const response = await axios.delete('/api/category/' + ID);
+    navigate ("/categories");
+  }
+
+  const editCategory = () => {
+
+  }
+ 
+    
+  return (
+      <div className="category-card">
+        <div className="category-info">
+            <div class="category-title">{TITLE}</div>
         </div>
+        <div className="category-spending-limit">Spending Limit: {SPENDINGLIMIT}</div>
+        <button className="category-card-button" onClick={editCategory}>Edit</button>
+        <button className="category-card-button" onClick={deleteCategory}>Delete </button>
+      </div>
     );
   }
 
