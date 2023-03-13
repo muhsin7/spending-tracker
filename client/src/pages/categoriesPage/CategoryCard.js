1817aedbf4c511a1737037025bb491f8bad3b02c
Huy Phan <duc.phan@kcl.ac.uk>
2023-03-13 17:11:41 +0000

    Add alerts for edit and delete

--- client/src/pages/categoriesPage/CategoryCard.js
+++ client/src/pages/categoriesPage/CategoryCard.js
@@ -1,18 +1,71 @@
 import React from "react";
-import { Navigate, useNavigate } from "react-router-dom";
 import Popup from "reactjs-popup";
 import "reactjs-popup/dist/index.css";
+import axios from "axios";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import { useToken } from "../../authentication/useToken";
+import { confirmAlert } from 'react-confirm-alert';
 
 function CategoryCard(props) {
 
-  const ID = props.category.id;
-  const TITLE = props.category.title;
+  const ID = props.category._id;
+  const TITLE = props.category.name;
   const SPENDINGLIMIT = props.category.spendingLimit
+  const [token, setToken] = useToken();
+  const navigate = useNavigate();
+
 
 
   const deleteCategory = async () => {
-    const response = await axios.delete('/api/category/' + ID);
-    navigate ("/categories");
+    try {
+      const response = await axios.delete('/api/category/' + ID, {
+        headers: {
+          "Authorization": "Bearer " + token
+        }
+      });
+      window.location.reload(false);
+
+  } catch (err) {
+      if(err.response) {
+          console.log(err.response);
+      } else if (err.message) {
+          console.log(err.message);
+      } else {
+          console.log(err);
+      }
+  }
+  }
+
+  function handleCategoryDelete() {
+    confirmAlert({
+      title: TITLE,
+      message: "Are you sure you want to delete this payment?",
+      buttons: [
+        {
+          label: "Yes",
+          onClick: () => deleteCategory()
+        },
+        {
+          label: "No"
+        }
+      ]
+    });
+  }
+
+  function handleCategoryEdit() {
+    confirmAlert({
+      title: TITLE,
+      message: "Please enter the new name for the category ",
+      buttons: [
+        {
+          label: "Confirm",
+          onClick: () => editCategory()
+        },
+        {
+          label: "Cancel"
+        }
+      ]
+    });
   }
 
   const editCategory = () => {
@@ -26,8 +79,8 @@ function CategoryCard(props) {
             <div class="category-title">{TITLE}</div>
         </div>
         <div className="category-spending-limit">Spending Limit: {SPENDINGLIMIT}</div>
-        <button className="category-card-button" onClick={editCategory}>Edit</button>
-        <button className="category-card-button" onClick={deleteCategory}>Delete </button>
+        <button className="category-card-button" onClick={handleCategoryEdit}>Edit</button>
+        <button className="category-card-button" onClick={handleCategoryDelete}>Delete </button>
       </div>
     );
   }
