c8e24b6291d9480d566c22a23f2d79f3bb0e95ca
Josh Wong <jorywo@gmail.com>
2023-03-16 22:24:29 +0000

    Fix account popup details

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,6 +4,9 @@ import { useAuth } from "../authentication/useAuth";
 import { useToken } from "../authentication/useToken";
 
 export default function Header(props) {
+  const [popup, setPopup] = useState(false)
+  const [changePassword, setChangePassword]=  useState(false)
+
   const [isAuth, setAuth] = props.auth;
   const [token, setToken] = useToken();
   const navigate = useNavigate();
@@ -17,7 +20,15 @@ export default function Header(props) {
     navigate("/");
   };
 
+  const toggleAccountPopup = () => {
+    setPopup(!popup)
+    if (!popup) {
+      setChangePassword(false)
+    }
+  };
+
   return (
+    <>
     <header className="header">
       <Link to="/" style={{ textDecoration: "none", color: "black" }}>
         <div className="logo">
@@ -30,20 +41,22 @@ export default function Header(props) {
           <div className="header-link">Contact Us</div>
         </li>
         <li>
-          <div className="header-link">About</div>
-        </li>
-        <li>
           <div className="header-link">Home</div>
         </li>
-        {isAuth ? (
+        {true ? (
+          <>
+          <li>
+            <div className="header-link" onClick={toggleAccountPopup}>Account</div>
+          </li>
           <li>
             <button
               className="btn btn-header btn-ghost error"
               onClick={onLogout}
-            >
+              >
               Log out
             </button>
           </li>
+          </>
         ) : (
           <>
             <Link to={"/register"}>
@@ -64,106 +77,60 @@ export default function Header(props) {
         )}
       </ul>
     </header>
+
+
+{popup && (
+  <>
+  <div className='overlay' onClick={toggleAccountPopup}></div>
+    <div className='popup'>
+    <h2>Account</h2>
+    <ul>
+      <li><h3>Name: </h3><div className='box'>Placeholder Name</div></li>
+      <li><h3>Email: </h3> <div className='box'>Placeholder Email</div></li>
+    </ul>
+
+    {/* <button className='btn password-btn' onClick={toggleChangePassword}>Change Password</button>
+
+    {changePassword && (
+        <form>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              name='password'
+              value={password}
+              placeholder='Enter new password'
+              onChange={onChange}
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='password2'
+              name='password2'
+              value={password2}
+              placeholder='Confirm new password'
+              onChange={onChange}
+              required
+            />
+          </div>
+          <button type='submit' className='btn submit-btn'>
+              Submit
+            </button>
+        </form>
+    )}
+
+ */}
+
+    <button className="btn close-btn" onClick={toggleAccountPopup}>
+      CLOSE
+    </button>
+  </div>
+  </>
+  )}
+  </>
   );
-  // const [popup, setPopup] = useState(false)
-  // const [changePassword, setChangePassword]=  useState(false)
-  // const [formData, setFormData] = useState({
-  //   password: '',
-  //   password2: '',
-  // })
-  // const {password, password2 } = formData
-
-  // const onChange = (e) => {
-  //   setFormData((prevState) => ({
-  //       ...prevState,
-  //       [e.target.name]: e.target.value,
-  //   }))
-  //   }
-
-  // const onLogout = () => {
-  //   window.alert("Logout")
-  // }
-
-  // const toggleAccountPopup = () => {
-  //   setPopup(!popup)
-  //   if (!popup) {
-  //     setChangePassword(false)
-  //   }
-  // }
-
-  // const toggleChangePassword = () => {
-  //   setChangePassword(!changePassword)
-  // }
-
-  // return (
-  //   <>
-  //     <header className='header'>
-  //       <div className='logo'>
-  //         <Link to='/'>Spending Tracker</Link>
-  //       </div>
-  //       <ul>
-  //         <li>
-  //           <button className='acc-btn' onClick={toggleAccountPopup}>
-  //             Account
-  //           </button>
-  //           <button className='btn btn-header' onClick={onLogout}>
-  //               Logout
-  //           </button>
-
-  //         </li>
-  //       </ul>
-  //     </header>
-
-  //     {popup && (
-  //     <>
-  //     <div className='overlay' onClick={toggleAccountPopup}></div>
-  //       <div className='popup'>
-  //       <h2>Account Details</h2>
-  //       <ul>
-  //         <li><h3>Name: </h3><div className='box'>Placeholder Name</div></li>
-  //         <li><h3>Email: </h3> <div className='box'>Placeholder Email</div></li>
-  //       </ul>
-  //       <button className='btn password-btn' onClick={toggleChangePassword}>Change Password</button>
-
-  //       {changePassword && (
-  //           <form>
-  //             <div className='form-group'>
-  //               <input
-  //                 type='password'
-  //                 className='form-control'
-  //                 id='password'
-  //                 name='password'
-  //                 value={password}
-  //                 placeholder='Enter new password'
-  //                 onChange={onChange}
-  //                 required
-  //               />
-  //             </div>
-  //             <div className='form-group'>
-  //               <input
-  //                 type='password'
-  //                 className='form-control'
-  //                 id='password2'
-  //                 name='password2'
-  //                 value={password2}
-  //                 placeholder='Confirm new password'
-  //                 onChange={onChange}
-  //                 required
-  //               />
-  //             </div>
-  //             <button type='submit' className='btn submit-btn'>
-  //                 Submit
-  //               </button>
-  //           </form>
-  //       )}
-
-  //       <button className="btn close-btn" onClick={toggleAccountPopup}>
-  //         CLOSE
-  //       </button>
-  //     </div>
-  //     </>
-  //     )}
-
-  //   </>
-  //   )
 }
