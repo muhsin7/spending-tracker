d5691511421fd32f5b1f175677388aa410662eb2
Raymond Cheung <raymond.cheung@kcl.ac.uk>
2023-03-17 00:19:29 +0000

    Implement sorting payments by date

--- client/src/pages/paymentsPage/PaymentsSortBy.js
+++ client/src/pages/paymentsPage/PaymentsSortBy.js
@@ -14,20 +14,70 @@ export default function PaymentsSortBy(props) {
   ];
 
   // Sort the payments so that the latest payment is displayed first
-  const ascendingCompare = (A, B) => {
+  function ascendingCompare(A, B) {
+    if (A === B) {
+      return 0;
+    } else {
+      return A < B ? -1 : 1;
+    }
+  }
+
+  // Sort the payments so that the latest payment is displayed first
+  function descendingCompare(A, B) {
     if (A === B) {
       return 0;
     } else {
       return A > B ? -1 : 1;
     }
-  };
+  }
 
-  props.payments.sort((a, b) =>
-    ascendingCompare(new Date(Date.parse(a.date)), new Date(Date.parse(b.date)))
-  );
+  function earliestDateFirst() {
+    props.setPayments([
+      ...props.payments.sort((a, b) =>
+        ascendingCompare(
+          new Date(Date.parse(a.date)),
+          new Date(Date.parse(b.date))
+        )
+      ),
+    ]);
+  }
+
+  function latestDateFirst() {
+    props.setPayments([
+      ...props.payments.sort((a, b) =>
+        descendingCompare(
+          new Date(Date.parse(a.date)),
+          new Date(Date.parse(b.date))
+        )
+      ),
+    ]);
+  }
 
   function confirmSortBy(e) {
     setSortBy(e.target.value);
+    switch (e.target.value) {
+      case "Category (a -> z)":
+        break;
+      case "Category (z -> a)":
+        break;
+      case "Title (a -> z)":
+        break;
+      case "Title (z -> a)":
+        break;
+      case "Price (lowest first)":
+        break;
+      case "Price (highest first)":
+        break;
+      case "Date (earliest first)":
+        earliestDateFirst();
+        break;
+      case "Date (latest first)":
+        latestDateFirst();
+        break;
+      default:
+        console.log("ERROR");
+        break;
+    }
   }
 
   return (
