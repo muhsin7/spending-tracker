a65fcf8948cc7ce582b500fc0c6908fb105579a7
Kai Ming <kai.tey@kcl.ac.uk>
2023-03-19 16:06:08 +0000

    Added a few achievements

--- server/models/achievementModel.js
+++ server/models/achievementModel.js
@@ -1,39 +1,5 @@
 const mongoose = require("mongoose");
 
-<<<<<<< HEAD
-const achievementSchema = mongoose.Schema(
-  {
-    id: {
-      type: Number,
-      required: true,
-      unique: true
-    },
-    name: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    description: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    goal: {
-      type: Number,
-      required: true
-    },
-    progress: {
-      type: Number,
-      required: true
-    },
-    completed: {
-      type: Boolean,
-      required: true,
-    },
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true
-=======
 // This model links users to achievements they have obtained
 const achievementModelSchema = mongoose.Schema(
   {
@@ -52,13 +18,8 @@ const achievementModelSchema = mongoose.Schema(
       default: Date.now,
       required: true,
       immutable: true
->>>>>>> 64a90b779f5bb92635a1a326fe18dc0b9d7b653d
     }
   }
 );
 
-<<<<<<< HEAD
-module.exports = mongoose.model("Achievement", achievementSchema);
-=======
 module.exports = mongoose.model("Achievement", achievementModelSchema);
->>>>>>> 64a90b779f5bb92635a1a326fe18dc0b9d7b653d
