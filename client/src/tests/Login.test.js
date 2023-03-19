4c85c9aa0bf67c0f68884cb67138a20e5a09828c
Huy Phan <duc.phan@kcl.ac.uk>
2023-03-19 18:48:25 +0000

    Add tests for the login page

--- client/src/tests/Login.test.js
+++ client/src/tests/Login.test.js
@@ -1,12 +1,54 @@
-import {Login} from "./Login"
+import Login from "../pages/Login";
 import {render} from "@testing-library/react";
+import user from "@testing-library/user-event"
+import React from "react";
+import { ReactDOM } from "react";
 
-test("renders login page correctly", () => {
+test("renders login page correctly", async () => {
     
-    const root = document.createElement("div");
-    ReactDOM.render(<Login />,root);
+    const onLoginClicked = jest.fn();
+
+    // beforeEach(() => {
+    //     onSubmit.mockClear();
+    //     render(<Login onSubmit={onLoginClicked} />);
+    // });
+
+    // const root = document.createElement("div");
+    // ReactDOM.render(<Login />,root);
 
     expect(root.querySelector("h1").textContent).toBe("Login");
+    expect(root.querySelector("#loginButton").textContent).toBe("Login");
+    expect(root.querySelector("#login-signup-link").textContent).toBe("Don't have an account?");
+
     
+    it('onLoginClicked is called when all fields pass validation', async () => {
+        user.type(getEmail(), 'johndoe@example.com');
+        user.type(getPassword(), '123');
+        clickSubmitButton();
+
+        await waitFor(() => {
+            expect(onLoginClicked).toHaveBeenCalledWith({
+                email: 'johndoe@example.com',
+                password: '123',
+            });
+        });
+
+    expect(onLoginClicked).toHaveBeenCalledTimes(1);
+    });
+
+    function clickSubmitButton() {
+        user.click(screen.getByRole('button'));
+    }
+
+    function getEmail() {
+        return screen.getByPlaceholderText('Email');
+    }
+
+    function getPassword() {
+        return screen.getByPlaceholderText('Password');
+    }
+
+
 });
+
 
