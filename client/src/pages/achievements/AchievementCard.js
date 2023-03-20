9c0c03d552b56328d439c711b8730da3bf02e69a
Raymond Cheung <raymond.cheung@kcl.ac.uk>
2023-03-20 13:26:18 +0000

    Add a description for each achievement

--- client/src/pages/achievements/AchievementCard.js
+++ client/src/pages/achievements/AchievementCard.js
@@ -4,7 +4,8 @@ export default function AchievementCard() {
   return (
     <div className="achievement-card">
       <div className="achievement-info">
-        <span>The best achievement of all time</span>
+        <span className="achievement-title">The best achievement of all time</span>
+        <span className="achievement-description">This may or may not be the best achievement of all time</span>
       </div>
       <AchievementCardProgress bgcolor={"#00B57f"} completed={80} />
     </div>
