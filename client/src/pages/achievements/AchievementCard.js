877480803224efbc9138ab23620d9d8d6f958094
Raymond Cheung <raymond.cheung@kcl.ac.uk>
2023-03-20 13:16:11 +0000

    Create achievement cards for achievements

--- client/src/pages/achievements/AchievementCard.js
+++ client/src/pages/achievements/AchievementCard.js
@@ -1,6 +1,12 @@
+import AchievementCardProgress from "./AchievementCardProgress";
+
 export default function AchievementCard() {
   return (
-    <div className="">
+    <div className="achievement-card">
+      <div className="achievement-info">
+        <span>The best achievement of all time</span>
+      </div>
+      <AchievementCardProgress bgcolor={"#00B57f"} completed={80} />
     </div>
   );
 }
