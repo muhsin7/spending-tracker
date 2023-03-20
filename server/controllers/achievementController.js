94aa122e55c8d3fabd5a7fcf3b5b1b8b36bc14e4
4v0n <navodhk@hotmail.com>
2023-03-20 15:01:51 +0000

    Change achievement endpoints to GET only

--- server/controllers/achievementController.js
+++ server/controllers/achievementController.js
@@ -18,32 +18,7 @@ const getAchievement = asyncHandler(async (req, res) => {
   }
 });
 
-// post new
-const createAchievement = asyncHandler(async (req, res) => {
-  try {
-    const {name, description, goal, progress, completed} = req.body;
-    const achievement = await Achievement.create({name, description, goal, progress, completed, userId: req.user.id});
-    res.status(201).json(achievement);
-  } catch (error) {
-    res.status(400).json({error: error.message});
-  }
-});
-
-// update
-const updateAchievement = asyncHandler(async (req, res) => {
-  try {
-    const {id} = req.params;
-    const achievement = await Achievement.findOneAndUpdate({id: id, userId: req.user.id}, {$inc: {progress: 1}});
-
-    res.status(200).json(achievement);
-  } catch (error) {
-    res.status(400).json({error: error.message});
-  }
-}); 
-
 module.exports = {
   getAchievements,
-  getAchievement,
-  createAchievement,
-  updateAchievement
+  getAchievement
 };
