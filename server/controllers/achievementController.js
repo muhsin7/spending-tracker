2614c61bc38256cbaf73e1f5c1d6609b6286e7a3
4v0n <navodhk@hotmail.com>
2023-03-20 16:35:11 +0000

    Return more usable data on achievement endpoints

--- server/controllers/achievementController.js
+++ server/controllers/achievementController.js
@@ -1,18 +1,39 @@
-const Achievement = require("../models/achievemetModel");
+const Achievement = require("../models/achievementModel");
+const AchievementSpec = require("../models/achievementSpecModel");
 const asyncHandler = require("express-async-handler");
+
+const buildAchievementJSON = async(achievement) => {
+  const spec = await AchievementSpec.findById(achievement.achievementSpecId);
+  return {
+    title: spec.title,
+    description: spec.description,
+    type: spec.type,
+    exp: spec.exp,
+    date: achievement.date,
+    userId: achievement.userId
+  };
+};
 
 // get all
 const getAchievements = asyncHandler(async (req, res) => {
   const achievements = await Achievement.find({userId: req.user.id});
-  res.status(200).json(achievements);
+
+  let items = [];
+
+  for (let i = 0; i < achievements.length; i++) {
+    const item = await buildAchievementJSON(achievements[i]);
+    items.push(item);
+  }
+
+  res.status(200).json(items);
 });
 
 // get specific
 const getAchievement = asyncHandler(async (req, res) => {
   try {
     const {id} = req.params;
-    const achievement = await Achievement.findById({id: id, userId: req.user.id});
-    res.status(200).json(achievement);
+    const achievement = await Achievement.findOne({_id: id, userId: req.user.id});
+    res.status(200).json(await buildAchievementJSON(achievement));
   } catch (error) {
     res.status(400).json({error: error.message});
   }
--- server/test/testAchievements.js
+++ server/test/testAchievements.js
@@ -3,7 +3,7 @@ const Achievement = require("../models/achievementModel");
 const AchievementSpec = require("../models/achievementSpecModel");
 const User = require("../models/userModel");
 
-const {flushDB, assertError} = require("./utils");
+const {flushDB, assertError, generateToken} = require("./utils");
 
 const chai = require("chai");
 const should = chai.should();
@@ -14,36 +14,43 @@ const app = require("../app");
 chai.use(chaiHttp);
 
 describe("Achievement tests", () => {
-  describe("Achievement model tests", () => {
-    let achievementSpec;
-    let user;
-    let achievement;
 
-    beforeEach(async() => {
-      await flushDB();
+  let achievementSpec;
+  let user;
+  let achievement;
+  let authToken;
+
+  before(async() => {
+    await flushDB();
     
-      const USER = {
-        name: "Jill",
-        email: "jill@example.com",
-        password: "123"
-      };
+    const USER = {
+      name: "Jill",
+      email: "jill@example.com",
+      password: "123"
+    };
 
-      const ACHIEVEMENT_SPEC = {
-        title: "categoryAchievement",
-        description: "An achievement",
-        exp: 10,
-        requirements: {
-          noCategories: {
-            target: 1
-          }
+    const ACHIEVEMENT_SPEC = {
+      title: "categoryAchievement",
+      description: "An achievement",
+      exp: 10,
+      requirements: {
+        noCategories: {
+          target: 1
         }
-      };
+      }
+    };
     
-      achievementSpec = await AchievementSpec.create(ACHIEVEMENT_SPEC);
-      user = await User.create(USER);
-      achievement = await Achievement.create({userId: user._id, achievementSpecId: achievementSpec._id});
-    });
+    achievementSpec = await AchievementSpec.create(ACHIEVEMENT_SPEC);
+    user = await User.create(USER);
+    achievement = await Achievement.create({userId: user._id, achievementSpecId: achievementSpec._id});
+    authToken = generateToken(user);
+  });
 
+  after(async() => {
+    await flushDB();
+  });
+
+  describe("Achievement model tests", () => {
     it("should create a valid achievement", async() => {
       //beforeEach should have succeded
       achievement.should.exist;
@@ -72,12 +79,33 @@ describe("Achievement tests", () => {
           should.not.exist(achievement, "Achivement should have been invalid");
         });
     });
+  });
 
-    it("should include extra fields", async() => {
-      const obj = await Achievement.findById(achievement._id);
+  describe("Achievement api tests", () => {
 
-      console.log(obj);
+    it("should get achievements", async() => {
+      const res = await chai.request(app)
+        .get("/api/achievement/")
+        .set("Authorization", ("Bearer " + authToken));
 
+      const obj = res.body[0];
+
+      res.should.have.status(200);
+      obj.should.have.property("title").eql(achievementSpec.title);
+      obj.should.have.property("description").eql(achievementSpec.description);
+      obj.should.have.property("type").eql(achievementSpec.type);
+      obj.should.have.property("exp");
+      obj.exp.should.be.equal(achievementSpec.exp);
+    });
+
+    it("should get a specific achievement", async() => {
+      const res = await chai.request(app)
+        .get("/api/achievement/" + achievement._id)
+        .set("Authorization", ("Bearer " + authToken));
+
+      const obj = res.body;
+
+      res.should.have.status(200);
       obj.should.have.property("title").eql(achievementSpec.title);
       obj.should.have.property("description").eql(achievementSpec.description);
       obj.should.have.property("type").eql(achievementSpec.type);
