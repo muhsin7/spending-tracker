96d131199b7d96313ddc9826d47664185613f013
Raymond Cheung <raymond.cheung@kcl.ac.uk>
2023-03-20 22:26:13 +0000

    Add a medal for achievements and change styles

--- client/src/pages/achievements/AchievementCard.js
+++ client/src/pages/achievements/AchievementCard.js
@@ -1,9 +1,12 @@
+import { FaMedal } from "react-icons/fa";
+
 export default function AchievementCard() {
   return (
     <div className="achievement-card">
       <div className="achievement-info">
         <span className="achievement-title">The best achievement of all time</span>
         <span className="achievement-description">This may or may not be the best achievement of all time</span>
+        <FaMedal />
       </div>
     </div>
   );
--- client/src/pages/achievements/Level.js
+++ client/src/pages/achievements/Level.js
@@ -1,8 +1,10 @@
 import ExpBar from "./ExpBar";
 
 export default function Level() {
-  const CURRENT_LEVEL = 8;
   const TOTAL_EXP = 880;
+  const CURRENT_LEVEL = Math.floor(TOTAL_EXP / 100);
+  const COMPLETED = TOTAL_EXP % 100;
+  const EXP_BAR_COLOR = "#00b57f";
 
   return (
     <div className="level-page">
@@ -10,7 +12,7 @@ export default function Level() {
       <span className="level-total-exp">Total exp gained: {TOTAL_EXP}</span>
       <div className="level-progress-section">
         <span className="level-progress-text">Progress until next level: </span>
-        <ExpBar bgcolor={"#00B57f"} completed={80} />
+        <ExpBar bgcolor={EXP_BAR_COLOR} completed={COMPLETED} />
       </div>
     </div>
   );
