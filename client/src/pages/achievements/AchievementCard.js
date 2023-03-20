7a27830cc7aaa9de6ee2f9a9a0a11fe367b8d3d2
Raymond Cheung <raymond.cheung@kcl.ac.uk>
2023-03-20 23:25:31 +0000

    Change medal and add disabled medal

--- client/src/pages/achievements/AchievementCard.js
+++ client/src/pages/achievements/AchievementCard.js
@@ -1,12 +1,14 @@
-import { FaMedal } from "react-icons/fa";
+import Medal from "../../images/Medal.png";
 
-export default function AchievementCard() {
+export default function AchievementCard(props) {
   return (
     <div className="achievement-card">
       <div className="achievement-info">
         <span className="achievement-title">The best achievement of all time</span>
         <span className="achievement-description">This may or may not be the best achievement of all time</span>
-        <FaMedal />
+        <img
+        className={props.isUnlocked ? "achievement-medal" : "achievement-disabled-medal"}
+        src={Medal} />
       </div>
     </div>
   );
--- client/src/pages/achievements/Achievements.js
+++ client/src/pages/achievements/Achievements.js
@@ -7,14 +7,14 @@ export default function Achievements() {
       <Level />
       <h1 className="achievements-header">Achievements</h1>
       <div className="achievement-section">
+        <AchievementCard isUnlocked={true} />
         <AchievementCard />
         <AchievementCard />
         <AchievementCard />
         <AchievementCard />
         <AchievementCard />
         <AchievementCard />
-        <AchievementCard />
-        <AchievementCard />
+        <AchievementCard isUnlocked={true} />
       </div>
     </div>
   );
