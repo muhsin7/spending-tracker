8e3fec9e3fd70a40c37ed11ec2de69263a41c244
Raymond Cheung <raymond.cheung@kcl.ac.uk>
2023-03-21 03:01:57 +0000

    Make all payments button on dashboard functional

--- client/src/pages/dashboard/transactions/TransactionsPreview.js
+++ client/src/pages/dashboard/transactions/TransactionsPreview.js
@@ -1,6 +1,8 @@
 import TransactionCard from "./TransactionCard";
+import { useNavigate } from "react-router-dom";
 
 export default function TransactionsPreview(props) {
+    const navigate = useNavigate();
     const rows = [];
     props.payments.forEach(e => {
     rows.push(<TransactionCard payment={e} key={e._id} />);
@@ -11,7 +13,7 @@ export default function TransactionsPreview(props) {
             <h2>Transactions</h2>
             <div className="dashboard-transactions dashboard-container">
                 {rows}
-                <div className="all-payments btn btn-header">All Payments</div>
+                <div className="all-payments btn btn-header" onClick={() => navigate("/payments")}>All Payments</div>
             </div>
         </div>
     )
