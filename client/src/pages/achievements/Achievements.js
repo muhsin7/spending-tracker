fb7ede8cf988e4f20443547d72088da0d1fe830d
Raymond Cheung <raymond.cheung@kcl.ac.uk>
2023-03-22 16:12:39 +0000

    Display the user's achievements from database

--- client/src/pages/achievements/Achievements.js
+++ client/src/pages/achievements/Achievements.js
@@ -17,38 +17,39 @@ export default function Achievements() {
         },
       })
       .then((res) => {
+        console.log(res.data);
         setAchievements(res.data);
       });
   }, []);
 
-  const ACHIEVEMENT_DUMMY = {
-    title: "The best achievement of all time",
-    description: "This may or may not be the best achievement of all time",
-    isUnlocked: true,
-  };
+  // const ACHIEVEMENT_DUMMY = {
+  //   title: "The best achievement of all time",
+  //   description: "This may or may not be the best achievement of all time",
+  //   isUnlocked: true,
+  // };
 
-  const ACHIEVEMENT_DUMMY2 = {
-    title: "¯\\_(ツ)_/¯",
-    description: "idk",
-    isUnlocked: false,
-  };
+  // const ACHIEVEMENT_DUMMY2 = {
+  //   title: "¯\\_(ツ)_/¯",
+  //   description: "idk",
+  //   isUnlocked: false,
+  // };
 
-  const ACHIEVEMENT_DUMMY3 = {
-    title: "Sure, here's some dummy text:",
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin nibh et enim bibendum, quis lobortis massa suscipit.`,
-    isUnlocked: false,
-  };
+  // const ACHIEVEMENT_DUMMY3 = {
+  //   title: "Sure, here's some dummy text:",
+  //   description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin nibh et enim bibendum, quis lobortis massa suscipit.`,
+  //   isUnlocked: false,
+  // };
 
-  const ACHIEVEMENTS = [
-    ACHIEVEMENT_DUMMY,
-    ACHIEVEMENT_DUMMY2,
-    ACHIEVEMENT_DUMMY3,
-  ];
+  // const ACHIEVEMENTS = [
+  //   ACHIEVEMENT_DUMMY,
+  //   ACHIEVEMENT_DUMMY2,
+  //   ACHIEVEMENT_DUMMY3,
+  // ];
 
   const cards = [];
-  ACHIEVEMENTS.forEach((e) => {
+  achievements.forEach((e) => {
     cards.push(
-      <AchievementCard isUnlocked={e.isUnlocked} achievement={e} key={e._id} />
+      <AchievementCard achievement={e} key={e.title} />
     );
   });
 
