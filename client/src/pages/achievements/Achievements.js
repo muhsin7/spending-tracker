c0cbb60711d1b710622ed98369f43be911d81efb
Raymond Cheung <raymond.cheung@kcl.ac.uk>
2023-03-22 15:40:26 +0000

    Allow new cards to be easily added to achievements

--- client/src/pages/achievements/Achievements.js
+++ client/src/pages/achievements/Achievements.js
@@ -5,32 +5,39 @@ export default function Achievements() {
   const ACHIEVEMENT_DUMMY = {
     title: "The best achievement of all time",
     description: "This may or may not be the best achievement of all time",
+    isUnlocked: true,
   };
 
   const ACHIEVEMENT_DUMMY2 = {
     title: "¯\\_(ツ)_/¯",
     description: "idk",
+    isUnlocked: false,
   };
 
   const ACHIEVEMENT_DUMMY3 = {
     title: "Sure, here's some dummy text:",
     description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin nibh et enim bibendum, quis lobortis massa suscipit.`,
+    isUnlocked: false,
   };
+
+  const ACHIEVEMENTS = [ACHIEVEMENT_DUMMY, ACHIEVEMENT_DUMMY2, ACHIEVEMENT_DUMMY3]
+
+  const cards = [];
+  ACHIEVEMENTS.forEach((e) => {
+    cards.push(
+      <AchievementCard
+        isUnlocked={e.isUnlocked}
+        achievement={e}
+        key={e._id}
+      />
+    );
+  });
 
   return (
     <div className="achievements-page">
       <Level />
       <h1 className="achievements-header">Achievements</h1>
-      <div className="achievement-section">
-        <AchievementCard isUnlocked={true} achievement={ACHIEVEMENT_DUMMY} />
-        <AchievementCard isUnlocked={false} achievement={ACHIEVEMENT_DUMMY2} />
-        <AchievementCard isUnlocked={false} achievement={ACHIEVEMENT_DUMMY3} />
-        <AchievementCard isUnlocked={false} achievement={ACHIEVEMENT_DUMMY3} />
-        <AchievementCard isUnlocked={false} achievement={ACHIEVEMENT_DUMMY} />
-        <AchievementCard isUnlocked={false} achievement={ACHIEVEMENT_DUMMY2} />
-        <AchievementCard isUnlocked={false} achievement={ACHIEVEMENT_DUMMY3} />
-        <AchievementCard isUnlocked={true} achievement={ACHIEVEMENT_DUMMY2} />
-      </div>
+      <div className="achievement-section">{cards}</div>
     </div>
   );
 }
